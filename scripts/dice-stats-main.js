3edbed1ff6b8887a01032efdc58a61c02c4b5f4a
jacobwojoski <jacobwojoski@gmail.com>
2023-04-29 15:01:17 -0400

    Update Global data

--- scripts/dice-stats-main.js
+++ scripts/dice-stats-main.js
@@ -832,10 +832,25 @@ class GlobalStatusPage extends FormApplication{
                 socket.executeForEveryone("push_sock", game.userId);
                 GLOBALFORMOBJ.render();
                 break;
-            case 'clearRollData':
+            case 'clearAllRollData':
                 title_txt = game.i18n.localize('DICE_STATS_TEXT.global_dialogs.clear_all_data.title');
                 context_txt = game.i18n.localize('DICE_STATS_TEXT.global_dialogs.clear_all_data.context');
-                const rollConfirmation = await Dialog.confirm({
+                const allClear = await Dialog.confirm({
+                    title: title_txt,
+                    content: context_txt,
+                    yes: () => {return true},
+                    no: () => {return false},
+                    defaultYes: false
+                    });
+                if(allClear){
+                    socket.executeForEveryone("clear_sock", {});
+                    DB_INTERACTION.clearDB();
+                }
+                break;
+            case 'clearAllLocalRollData':
+                title_txt = game.i18n.localize('DICE_STATS_TEXT.global_dialogs.clear_all_local_data.title');
+                context_txt = game.i18n.localize('DICE_STATS_TEXT.global_dialogs.clear_all_local_data.context');
+                const localClear = await Dialog.confirm({
                     title: title_txt,
                     content: context_txt,
                     yes: () => {return true},
@@ -843,12 +858,14 @@ class GlobalStatusPage extends FormApplication{
                     defaultYes: false
                     });
 
-                if (rollConfirmation) {
+                if (localClear) {
                     socket.executeForEveryone("clear_sock", {});
-
-                    title_txt = game.i18n.localize('DICE_STATS_TEXT.global_dialogs.clear_db.title');
-                    context_txt = game.i18n.localize('DICE_STATS_TEXT.global_dialogs.clear_db.context');
-                    dbconfirmation = await Dialog.confirm({
+                }
+                break;
+            case 'clearAllDBrollData':
+                title_txt = game.i18n.localize('DICE_STATS_TEXT.global_dialogs.clear_all_db_data.title');
+                    context_txt = game.i18n.localize('DICE_STATS_TEXT.global_dialogs.clear_all_db_data.context');
+                    const dbClear = await Dialog.confirm({
                         title: title_txt,
                         content: context_txt,
                         yes: () => {return true},
@@ -856,27 +873,9 @@ class GlobalStatusPage extends FormApplication{
                         defaultYes: false
                         });
     
-                    if (dbconfirmation) {
+                    if (dbClear) {
                         DB_INTERACTION.clearDB();
                     }
-
-                    GLOBALFORMOBJ.render();
-                }
-                break;
-            case 'clearDB':
-                title_txt = game.i18n.localize('DICE_STATS_TEXT.global_dialogs.clear_db.title');
-                context_txt = game.i18n.localize('DICE_STATS_TEXT.global_dialogs.clear_db.context');
-                dbconfirmation = await Dialog.confirm({
-                    title: title_txt,
-                    content: context_txt,
-                    yes: () => {return true},
-                    no: () => {return false},
-                    defaultYes: false
-                    });
-
-                if (dbconfirmation) {
-                    DB_INTERACTION.clearDB();
-                }
                 break;
             case 'd2checkbox':
                 CLASSOBJ.GLOBAL_DICE_CHECKBOXES[0] = !CLASSOBJ.GLOBAL_DICE_CHECKBOXES[0];
