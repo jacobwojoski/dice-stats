e2a97d3df9607a6f49cb315294ada022c1a75131
jacobwojoski <jacobwojoski@gmail.com>
2024-03-13 20:36:32 -0400

    update some comments

--- scripts/classes/appdatastorage/dice-stats-rollmsginfo.js
+++ scripts/classes/appdatastorage/dice-stats-rollmsginfo.js
@@ -1,5 +1,6 @@
     /* DIE_TYPE, ROLL_TYPE, ROLL_VALUE, IS_BLIND, DEG_SUCCESS*/
     /* Any Negative value is invalid */
+    /* Storage of any info related to the DICE that in the message (Trim out all the useless bits)*/
     class DS_DIE_ROLL_INFO {
         DieType=    -1; //{DIE_TYPE}
         RollType=   -1; //{ROLL_TYPE}
@@ -20,6 +21,7 @@
      * When a message is made in chat it can include multiple rolls, and multiple dice of each type
      * EX: "/r 2d20 + 1d10 + 2d4kh" is a valid roll inside the message. There could even be multiple rolls.
      * Storage Holds Roll Info and an array of Dice Info, Also helps if Hit and Miss's Require multiple dice
+     * Trim out all the useless bits from the MSG.ROLL obj
      */
     class DS_ROLL_INFO {
         DiceInfo = []; //{DS_DIE_ROLL_INFO}
