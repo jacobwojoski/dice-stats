78896dce6d49f15dd7cfb66f20d4ea8e765e2522
jacobwojoski <jacobwojoski@gmail.com>
2024-03-13 20:49:14 -0400

    update factory fn call

--- scripts/classes/systemMessageParsers/dice-stats-messageparserfactory.js
+++ scripts/classes/systemMessageParsers/dice-stats-messageparserfactory.js
@@ -1,27 +1,13 @@
 class MESSAGE_PARSER_FACTORY  {
     
     /* Create a message parser object bassed on the current system Different systems need to do different stuff to parse the msg*/
-    static createMessageParser()
+    static createMessageParser(systemid)
     {
-        switch(game.system.id)
-        {
-            case "pf1e":
-                return new PF1E_SYSTEM_MESSAGE_PARSER;
-            case "pf2e" :
-                return new PF2E_SYSTEM_MESSAGE_PARSER;
-            case "dnd5e" :
-                return new DND5E_SYSTEM_MESSAGE_PARSER;
-            case "dragonbane" :
-                return new DRAGONBANE_SYSTEM_MESSAGE_PARSER;
-
-            default :
-                return new GENERIC_SYSTEM_MESSAGE_PARSER;
+        if(!systemid){
+            systemid = game.system.id
         }
-    }
 
-    static createMessageParser(system)
-    {
-        switch(system)
+        switch(systemid)
         {
             case "pf1e":
                 return new PF1E_SYSTEM_MESSAGE_PARSER;
@@ -31,7 +17,9 @@ class MESSAGE_PARSER_FACTORY  {
                 return new DND5E_SYSTEM_MESSAGE_PARSER;
             case "dragonbane" :
                 return new DRAGONBANE_SYSTEM_MESSAGE_PARSER;
-
+            
+            case "daggerheart" :
+            case "mcdmrpg" : 
             default :
                 return new GENERIC_SYSTEM_MESSAGE_PARSER;
         }
