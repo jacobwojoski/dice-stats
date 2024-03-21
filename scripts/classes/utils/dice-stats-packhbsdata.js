401c986df4ebdebaf7998f35b347a15fb12ea526
jacobwojoski <jacobwojoski@gmail.com>
2024-03-20 21:10:41 -0400

    start new datapackager

--- scripts/classes/utils/dice-stats-packhbsdata.js
+++ scripts/classes/utils/dice-stats-packhbsdata.js
@@ -1,5 +1,11 @@
 class PACKAGE_HIT_MISS_DATA {
-
+    // Create a hit and miss object that can be used by the hit-miss.hbs 
+    static package_hit_miss_info(HitMissInfoAry) {
+        //new HIT_MISS_HBS_DATA()
+        
+        // Create a hit miss obj dependent on the game system
+        
+    }
 }
 
 class PACKAGE_ALL_STATS_DATA {
@@ -11,5 +17,5 @@ class PACKAGE_TWODX_STATS_DATA {
 }
 
 class PACKAGE_D20_STATS_DATA {
-
+    
 }
