e2a5402d92f734cbfb1bb062cb6ba5eebe53229e
Jacob <124112219+jacobwojoski@users.noreply.github.com>
2025-05-28 09:19:06 -0400

    Update chartData.ts
    Create Generic And Extended chart obj

--- src/ts/dataModel/displayData/chartData.ts
+++ src/ts/dataModel/displayData/chartData.ts
@@ -1,29 +1,22 @@
 /**
  * Generic display data for a chart with all data values being integers
  */
-export class IntChartData 
+export class IntChartData extends ChartData
 {
-    isDisplayed = false;
-
-    chartTitle = '';    //
-    chartId = '';       // 
-    chartData = {};     // Chart.js Data object
-
     totalRolls = 0;
     mean = 0;
     median = 0;
     mode = 0;
-
     streak = '';
 
     constructor(
-        title_in:string, chart_data_in:any, total_rolls_in:number,
+        is_displayed_in: boolean, title_in:string, id_in:string,
+        chart_data_in:any, total_rolls_in:number,
         mean_in:number, median_in:number, mode_in:number,
-        streak_in: string, is_displayed_in: boolean
+        streak_in: string
     ){
-        this.isDisplayed = is_displayed_in;
-        this.chartTitle = title_in;
-        this.chartData = chart_data_in;
+        this.super(is_displayed_in, title_in, id_in, chart_data_in)
+        
         this.totalRolls = total_rolls_in;
         this.mean = mean_in;
         this.median = median_in;
@@ -31,3 +24,23 @@ export class IntChartData
         this.streak = streak_in;
     }
 }
+
+/**
+* Generic Chart Display Info
+*/
+export class ChartData
+{
+    isDisplayed = false;
+
+    chartTitle = '';    //
+    chartId = '';       // 
+    chartData = {};     // Chart.js Data object
+
+    constructor(is_displayed_in: boolean, title_in:string, id_in:string, chart_data_in:any){
+        this.isDisplayed = is_displayed_in;
+        this.chartTitle = title_in;
+        this.charId = id_in;
+        this.chartData = chart_data_in;
+    }
+}
+        
